fe878cc916b7f98a6d37a1f97cb38fa9d0b6188c
William Crews <crewswh@gmail.com>
2021-11-10 13:45:41 -0500

    added from ECP4

---
+++ wtd/src/pages/mainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainPage = () => {
+    return (
+        <div className='App'>
+            <h1>mainPage</h1>
+        </div>
+    )
+}
+
+export default mainPage
