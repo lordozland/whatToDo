35aaa82f1a6743b2ce901081fe915ff7c3de09b2
William Crews <crewswh@gmail.com>
2021-11-10 13:48:04 -0500

    adding router

--- wtd/src/App.js
+++ wtd/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import mainPage from './pages/mainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    <Switch>
+      <Route path="/" exact component={mainPage} />
+    </Switch>
     </div>
+    </Router>
   );
 }
 
