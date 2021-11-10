9dd706aeb310fa0803a0787a2528426340ae40b6
William Crews <crewswh@gmail.com>
2021-11-10 17:05:49 -0500

    might keep working if I can find the time tomorrow

--- wtd/src/pages/mainPage.jsx
+++ wtd/src/pages/mainPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const styles = {
+    borders: {
+        border: 'solid'
+    }
+}
+
 const mainPage = () => {
     return (
-        <div className='mainPage'>
-            <div>Hello!</div>
+        <div >
+            <div className='mainPage text-5xl'>Hello!</div>
+            <div style={styles.borders} className='m-6'>What to put here?</div>
         </div>
+        
     )
 }
 
