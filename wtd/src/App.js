5cd7287cc30a1749a23ae9e31c056c35a447278b
William Crews <crewswh@gmail.com>
2021-11-10 14:17:36 -0500

    solution - cap mainpage

--- wtd/src/App.js
+++ wtd/src/App.js
@@ -4,13 +4,13 @@ import './index.css';
 
 // Pages
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import mainPage from './pages/mainPage';
+import MainPage from './pages/mainPage';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<mainPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
   );
--- wtd/src/pages/mainPage.jsx
+++ wtd/src/pages/mainPage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const mainPage = () => {
     return (
-        <div className='mainPage'>
+        <div className='App'>
             <h1>mainPage</h1>
         </div>
     )
 }
 
-export default mainPage
+export default mainPage;
