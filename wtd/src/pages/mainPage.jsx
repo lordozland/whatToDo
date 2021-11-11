b6cc2e6ec11b2ace98b5297d4ff9e4bea864429d
William Crews <crewswh@gmail.com>
2021-11-10 19:24:11 -0500

    I love my family

--- wtd/src/pages/mainPage.jsx
+++ wtd/src/pages/mainPage.jsx
@@ -9,11 +9,12 @@ const styles = {
 const mainPage = () => {
     return (
         <div >
-            <div className='mainPage text-5xl'>Hello!</div>
-            <div style={styles.borders} className='m-6'>What to put here?</div>
+            <div className='mainPage text-5xl m-6'>Hello!</div>
+            <div style={styles.borders} className='m-6 p-8'>I love my FAMILY!!!!</div>
         </div>
         
     )
 }
 
 export default mainPage;
+
